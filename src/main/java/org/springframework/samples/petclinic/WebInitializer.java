b90ea983a854ca053c7a4c6dfaf06e8aeab2f456
Scythd <74152112+Scythd@users.noreply.github.com>
2021-07-01 22:10:18 +0400

    Update WebInitializer.java

--- src/main/java/org/springframework/samples/petclinic/WebInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/WebInitializer.java
@@ -1,14 +1,14 @@
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class WebInitializer extends SpringBootServletInitializer {
 
     @Override
     @Bean
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.sources(PetClinicApplication.class);
     }
 
 }
